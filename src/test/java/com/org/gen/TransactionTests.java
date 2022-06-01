d3397e76870f7cb6893063cbc01677c56dd74212
Shivesh Bharti <shiveshbharti7252@gmail.com>
2022-06-01 13:52:04 +0530

    nothing

--- src/test/java/com/org/gen/TransactionTests.java
+++ src/test/java/com/org/gen/TransactionTests.java
@@ -1,7 +1,6 @@
 package com.org.gen;
 
 import static org.assertj.core.api.Assertions.assertThat;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
