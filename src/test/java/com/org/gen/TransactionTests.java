4798a31159522ea04e2cc6747c0845f825f7d91e
Shivesh Bharti <shiveshbharti7252@gmail.com>
2022-06-01 13:59:33 +0530

    Added a comment in test file

--- src/test/java/com/org/gen/TransactionTests.java
+++ src/test/java/com/org/gen/TransactionTests.java
@@ -30,6 +30,7 @@ public class TransactionTests {
 	
 	@Test
 	public void testCreateTransaction() {
+	//this function helps in testing my transaction api
 		Transaction transaction = new Transaction();
 		transaction.setAccount_id(123);
 //		transaction.setTrans_id(1002);
